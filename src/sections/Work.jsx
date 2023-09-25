5be8f018cf2d3baf9ef508ba1862c67f97eae54c
PVSKR <skr.polu@hotmail.com>
2023-09-25 11:07:05 +0530

    work.jsx aside part is  mounting

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { work } from "../constants";
 
 const Work = () => {
   return (
     <section id="work">
-      <h2>Work</h2>
+      <div>
+        <aside class="flex self-stretch grid-cols-1 w-40 flex-col items-center">
+          <h2 className="hidden">Work</h2>
+          <ul className="p-0 border-l border-white ml-[3rem] list-none grid gap-[var(--fs-200)]">
+            {work.map((item) => (
+              <li className="relative pl-8 py-2 ">
+                <p class="hidden md:block text-2xl font-bold mt-2 mb-2"></p>
+                <img
+                  src={item.src}
+                  alt={item.alt}
+                  className="h-[1.5rem] w-[1.5rem]"
+                ></img>
+                <span className="ml-2 text-white">{item.name}</span>
+              </li>
+            ))}
+          </ul>
+        </aside>
+      </div>
     </section>
   );
 };
