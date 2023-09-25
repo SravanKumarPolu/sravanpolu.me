72f4c7e31dd871935e23995d98c06cbee752865a
PVSKR <skr.polu@hotmail.com>
2023-09-25 11:46:07 +0530

    Within the 'work.jsx' file, there is an 'aside' element with icons that reveal their names when hovered over..

--- src/sections/Work.jsx
+++ src/sections/Work.jsx
@@ -2,21 +2,33 @@ import React from "react";
 import { work } from "../constants";
 
 const Work = () => {
+  const [hoveredIcon, setHoveredIcon] = React.useState(null);
   return (
     <section id="work">
       <div>
         <aside class="flex self-stretch grid-cols-1 w-40 flex-col items-center">
           <h2 className="hidden">Work</h2>
-          <ul className="p-0 border-l border-white ml-[3rem] list-none grid gap-[var(--fs-200)]">
-            {work.map((item) => (
-              <li className="relative pl-8 py-2 ">
-                <p class="hidden md:block text-2xl font-bold mt-2 mb-2"></p>
+          <ul className="p-0 border-l border-black ml-[3rem] list-none grid gap-[var(--fs-200)]">
+            {work.map((icon) => (
+              <li className="relative pl-8 py-2">
+                <p className="hidden md:block text-2xl font-bold mt-2 mb-2"></p>
                 <img
-                  src={item.src}
-                  alt={item.alt}
-                  className="h-[1.5rem] w-[1.5rem]"
-                ></img>
-                <span className="ml-2 text-white">{item.name}</span>
+                  src={icon.src}
+                  alt={icon.alt}
+                  className="h-[1.5rem] w-[1.5rem] relative"
+                  onMouseEnter={() => setHoveredIcon(icon)}
+                  onMouseLeave={() => setHoveredIcon(null)}
+                />
+                {hoveredIcon === icon && (
+                  <div
+                    className=" flex absolute text-gray-800
+                   bg-gray-200 text-xs px-2 ml-[4rem]
+                   py-1 mt-20 rounded opacity-100 
+                    transform -translate-x-1/2"
+                  >
+                    {icon.name}
+                  </div>
+                )}
               </li>
             ))}
           </ul>
