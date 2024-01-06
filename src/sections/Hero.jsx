a8eae3c697512d315e76f03a6b12179871cbb87b
PVSKR <skr.polu@hotmail.com>
2024-01-07 00:26:54 +0530

    hero section motion tag applied to img.

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import headerLogo from "../assets/images/headerLogo.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
     <section
       id="home"
       className="w-full flex flex-col items-center justify-center min-h-screen gap-10 max-container ">
-      <div className="relative py-10  top-20 flex flex-col justify-center items-center m-4 w-4/5 max-sm:p-5 bg-gradient-to-br from-[#ff6b6b] to-[#ffa07a] rounded">
-        <img
+      <motion.div
+        className="relative py-10  top-20 flex
+       flex-col justify-center items-center m-4 w-4/5
+        max-sm:p-5 bg-gradient-to-br from-[#ff6b6b] to-[#ffa07a] rounded"
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.9 }}
+        variants={{
+          hidden: { opacity: 0, x: -100 },
+          visible: { opacity: 1, x: 0 },
+        }}>
+        <motion.img
           className="relative cursor-pointer p-1 border-4 rounded-full object-cover filter brightness-[75%] hover:border-l-green-500 hover:border-t-green-500 hover:border-b-red-400 hover:brightness-100 transition duration-300"
           src={headerLogo}
           alt="logo"
           width={200}
           height={200}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0 }}
+          variants={{
+            hidden: { opacity: 0, y: -100 },
+            visible: { opacity: 1, y: 0 },
+          }}
         />
-      </div>
+      </motion.div>
 
       <div className="relative flex flex-col justify-center items-center w-full max-xl:px-10 pt-10">
         <h3 className="pt-6 font-palanquin text-4xl md:text-5xl max-sm:text-2xl max-sm:leading-[2] font-semibold text-center text-gray-700 ">
