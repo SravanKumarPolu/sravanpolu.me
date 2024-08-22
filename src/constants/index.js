a2ef7ee17a7fd77fa3f397d6be98d4a0bd531fab
PVSKR <skr.polu@hotmail.com>
2024-08-22 23:41:33 +0530

    styles modified.

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,20 +8,6 @@ import css from "../assets/icons/css.svg";
 import tailwind from "../assets/icons/tailwind.svg";
 import bootstrap from "../assets/icons/bootstrap.svg";
 import jsfiddle from "../assets/icons/jsfiddle.svg";
-// import {
-//   bigShoe1,
-//   bigShoe2,
-//   bigShoe3,
-//   customer1,
-//   customer2,
-//   shoe4,
-//   shoe5,
-//   shoe6,
-//   shoe7,
-//   thumbnailShoe1,
-//   thumbnailShoe2,
-//   thumbnailShoe3,
-// } from "../assets/images";
 
 export const navLinks = [
   { href: "#home", label: "Home" },
@@ -30,85 +16,6 @@ export const navLinks = [
   { href: "#footer", label: "Contact me" },
 ];
 
-// export const shoes = [
-//   {
-//     thumbnail: thumbnailShoe1,
-//     bigShoe: bigShoe1,
-//   },
-//   {
-//     thumbnail: thumbnailShoe2,
-//     bigShoe: bigShoe2,
-//   },
-//   {
-//     thumbnail: thumbnailShoe3,
-//     bigShoe: bigShoe3,
-//   },
-// ];
-
-// export const statistics = [
-//   { value: "1k+", label: "Brands" },
-//   { value: "500+", label: "Shops" },
-//   { value: "250k+", label: "Customers" },
-// ];
-
-// export const products = [
-//   {
-//     imgURL: shoe4,
-//     name: "Nike Air Jordan-01",
-//     price: "₹2000",
-//   },
-//   {
-//     imgURL: shoe5,
-//     name: "Nike Air Jordan-10",
-//     price: "₹2200",
-//   },
-//   {
-//     imgURL: shoe6,
-//     name: "Nike Air Jordan-100",
-//     price: "₹2520",
-//   },
-//   {
-//     imgURL: shoe7,
-//     name: "Nike Air Jordan-001",
-//     price: "₹3400",
-//   },
-// ];
-
-// export const services = [
-//   {
-//     imgURL: truckFast,
-//     label: "Free shipping",
-//     subtext: "Enjoy seamless shopping with our complimentary shipping service.",
-//   },
-//   {
-//     imgURL: shieldTick,
-//     label: "Secure Payment",
-//     subtext:
-//       "Experience worry-free transactions with our secure payment options.",
-//   },
-//   {
-//     imgURL: support,
-//     label: "Love to help you",
-//     subtext: "Our dedicated team is here to assist you every step of the way.",
-//   },
-// ];
-
-// export const reviews = [
-//   {
-//     imgURL: customer1,
-//     customerName: "Sravan Kumar PV",
-//     rating: 4.5,
-//     feedback:
-//       "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!",
-//   },
-//   {
-//     imgURL: customer2,
-//     customerName: "Lota Mongeskar",
-//     rating: 4.5,
-//     feedback:
-//       "The product not only met but exceeded my expectations. I'll definitely be a returning customer!",
-//   },
-// ];
 export const courses = [
   {
     courseName: "CSS",
